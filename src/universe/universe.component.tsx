1bb5ea10e1dc9b64a8dd6446808b5b6e81bc42ba
Mike Lee <mike.lee816@gmail.com>
2022-02-21 14:10:40 -0500

    Move App component's functionality to Universe component

---
+++ src/universe/universe.component.tsx
@@ -0,0 +1,222 @@
+import React, { useEffect, useRef } from 'react';
+
+import './universe.styles.scss';
+
+import Planet from '../planet';
+
+const Universe: React.FC = () => {
+    const canvasRef = useRef(null);
+    let canvas: HTMLCanvasElement | null;
+    let ctx: CanvasRenderingContext2D | null;
+    let planet1: Planet;
+    let planet2: Planet;
+    let planet3: Planet;
+    let planet4: Planet;
+    let planet5: Planet;
+    let planet6: Planet;
+    let planets: Planet[];
+    const WIDTH = window.innerWidth;
+    const HEIGHT = window.innerHeight - 200;
+    const RED = '#c76161';
+    const BLUE = '#7490e3';
+    const GREEN = '#56ba6f';
+    const YELLOW = '#d1d44e';
+
+    useEffect(() => {
+        if (canvasRef.current !== undefined) canvas = canvasRef.current;
+        if (canvas) ctx = canvas.getContext('2d');
+    
+        planet1 = new Planet(9000000000000, 10, 400, 400, 0, 0, YELLOW);
+        planet2 = new Planet(30000000000, 10, 0, 0, 0, .5, RED);
+        planet3 = new Planet(10000000000, 10, 600, 0, -.5, 0, GREEN);
+        planet4 = new Planet(90000000000, 10, 0, 700, .8, 0, GREEN);
+        planet5 = new Planet(10000000000, 10, 600, 800, 0, -1.5, RED);
+        planet6 = new Planet(10000000000, 10, 800, 0, 0, .5, BLUE);
+    
+        planets = [planet1, planet2, planet3, planet4, planet5, planet6];
+    
+        requestAnimationFrame(refresh);
+      }, []);
+    
+      const refresh = () => {
+        ctx?.clearRect(0, 0, WIDTH, HEIGHT);
+    
+        planets.forEach(planet => {
+          const otherPlanets = planets.filter(otherPlanet => otherPlanet !== planet);
+    
+          const deltaVelocities = calcDeltaVelocities(planet, otherPlanets);
+    
+          planet.updateVelocity(deltaVelocities.xVelocity, deltaVelocities.yVelocity);
+    
+          planet.move();
+          if (ctx) planet.draw(ctx);
+        });
+    
+        requestAnimationFrame(refresh);
+      }
+    
+      const calcDeltaVelocities = (planet: Planet, otherPlanets: Planet[]) => {
+        const gravityForces = calcGravityForces(planet, otherPlanets);
+    
+        const gravityAccelerations = calcGravityAccelerations(planet, gravityForces);
+    
+        const directions = calcDirections(planet, otherPlanets);
+    
+        const accelerationsAndDirections = combineAccelerationsAndDirections(gravityAccelerations, directions);
+    
+        const velocities = calcVelocities(accelerationsAndDirections);
+    
+        const velocitiesSum = sumVelocities(velocities);
+    
+        return velocitiesSum;
+      }
+    
+      const calcGravityForces = (planet: Planet, otherPlanets: Planet[]) => {
+        const m1 = planet.mass;
+    
+        const gravityForces = otherPlanets.map(otherPlanet => {
+          const m2 = otherPlanet.mass;
+    
+          const deltaX = Math.abs(otherPlanet.x - planet.x);
+          const deltaY = Math.abs(otherPlanet.y - planet.y);
+          const distance = Math.sqrt((deltaX ** 2) + (deltaY ** 2));
+    
+          const gravity = calcGravity(m1, m2, distance);
+    
+          return gravity
+        });
+    
+        return gravityForces;
+      }
+    
+      const calcGravity = (m1: number, m2: number, r: number) => {
+        const G = 6.674 * (10 ** -11);
+        const f = G * ((m1 * m2) / (r ** 2));
+    
+        return f;
+      }
+    
+      const calcGravityAccelerations = (planet: Planet, gravityForces: number[]) => {
+        const accelerations = gravityForces.map(gravityForce => {
+          return calcAcceleration(planet.mass, gravityForce);
+        });
+    
+        return accelerations;
+      }
+    
+      const calcAcceleration = (planetMass: number, gravity: number) => {
+        // a = f/m
+        const acceleration = gravity / planetMass;
+    
+        return acceleration;
+      }
+    
+      const calcDirections = (planet: Planet, otherPlanets: Planet[]) => {
+        const directions = otherPlanets.map(otherPlanet => {
+          const x1 = planet.x;
+          const y1 = planet.y;
+          const x2 = otherPlanet.x;
+          const y2 = otherPlanet.y;
+    
+          // + === move positive (right)
+          // - === move negative (left)
+          // 0 === stay still
+          const xDirection = x2 - x1;
+    
+          // + === move positive (down)
+          // - === move negative (up)
+          // 0 === stay still
+          const yDirection = y2 - y1;
+    
+          return {
+            xDirection,
+            yDirection
+          }
+        });
+    
+        return directions;
+      }
+    
+      const combineAccelerationsAndDirections = (accelerations: number[], directions: { xDirection: number, yDirection: number }[]) => {
+        const accelerationsAndDirections = [];
+        for (let i = 0; i < accelerations.length; i++) {
+          accelerationsAndDirections.push({
+            acceleration: accelerations[i],
+            directions: directions[i]
+          });
+        }
+    
+        return accelerationsAndDirections;
+      }
+    
+      const calcVelocities = (accelerationsAndDirections: { acceleration: number, directions: { xDirection: number, yDirection: number } }[]) => {
+        const velocities = accelerationsAndDirections.map(accelerationAndDirection => {
+          const { acceleration, directions } = accelerationAndDirection;
+    
+          // find y:x ratio
+          let yPercentOfX;
+          if (directions.xDirection === 0) {
+            // Needed because you can't divide by 0
+            yPercentOfX = 1;
+          } else {
+            yPercentOfX = Math.abs(directions.yDirection / directions.xDirection);
+          }
+    
+          // convert gravitational acceleration (a) to x and y velocities
+          let xVelocity = 0;
+          let yVelocity = 0;
+    
+          // y = yPercentOfX * x;
+    
+          // x^2 + y^2 = a^2
+          // x^2 + (yPercentOfX*x)^2 = a^2
+          // x + (sqrt(yPercentOfX) * x) = a
+          // (1 + sqrt(yPercentofX)) * x = a
+          // x = a / (1 + sqrt(yPercentOfX) 
+    
+    
+          xVelocity = acceleration / (1 + Math.sqrt(yPercentOfX));
+          yVelocity = yPercentOfX * xVelocity;
+    
+          // velocity is negative if direction is negative
+          if (directions.xDirection < 0) {
+            xVelocity *= -1;
+          }
+    
+          // velocity is negative if direction is negative
+          if (directions.yDirection < 0) {
+            yVelocity *= -1;
+          }
+    
+          return {
+            xVelocity,
+            yVelocity
+          }
+        });
+    
+        return velocities;
+      }
+    
+      const sumVelocities = (velocities: { xVelocity: number, yVelocity: number }[]) => {
+        let xVelocity = 0;
+        let yVelocity = 0;
+    
+        velocities.forEach(velocity => {
+          xVelocity += velocity.xVelocity;
+          yVelocity += velocity.yVelocity;
+        });
+    
+        return {
+          xVelocity,
+          yVelocity
+        }
+      }
+
+    return (
+        <div className='universe'>
+            <canvas ref={canvasRef} id='canvas' width={WIDTH} height={HEIGHT}></canvas>
+        </div>
+    );
+}
+
+export default Universe;
