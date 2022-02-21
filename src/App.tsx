3172af0d237cedb92b0f452971dd7a98eb008cdb
Mike Lee <mike.lee816@gmail.com>
2022-02-21 16:57:42 -0500

    Add Controls component and addPlanet() function

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,25 @@ import React, { useState } from 'react';
 
 import './App.scss';
 
+import Controls from './controls/controls.component';
 import Universe from './universe/universe.component';
 
 import Planet from './planet';
 
 function App() {
   const [planets, setPlanets] = useState<Planet[]>([]);
+
+  const addPlanet = (planet: Planet) => {
+    setPlanets([
+      ...planets,
+      planet
+    ]);
+  }
   
   return (
     <div className='app'>
       <Universe planets={planets} />
+      <Controls addPlanet={addPlanet} />
     </div>
   );
 }
