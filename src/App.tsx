460dfbff7647174fe5b4fa2272cbce8b51e87003
Mike Lee <mike.lee816@gmail.com>
2022-02-22 14:02:28 -0500

    Change indent to four spaces instead of two

--- src/App.tsx
+++ src/App.tsx
@@ -8,21 +8,21 @@ import Universe from './universe/universe.component';
 import Planet from './planet';
 
 function App() {
-  const [planets, setPlanets] = useState<Planet[]>([]);
+    const [planets, setPlanets] = useState<Planet[]>([]);
 
-  const addPlanet = (planet: Planet) => {
-    setPlanets([
-      ...planets,
-      planet
-    ]);
-  }
-  
-  return (
-    <div className='app'>
-      <Universe planets={planets} />
-      <Controls addPlanet={addPlanet} />
-    </div>
-  );
+    const addPlanet = (planet: Planet) => {
+        setPlanets([
+            ...planets,
+            planet
+        ]);
+    }
+    
+    return (
+        <div className='app'>
+            <Universe planets={planets} />
+            <Controls addPlanet={addPlanet} />
+        </div>
+    );
 }
 
 export default App;
