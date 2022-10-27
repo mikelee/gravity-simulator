147b305113bf49161441a89408d340d544c84548
Mike Lee <mike.lee816@gmail.com>
2022-10-27 19:16:19 -0400

    Redesign Controls layout

--- src/controls/controls.component.tsx
+++ src/controls/controls.component.tsx
@@ -20,6 +20,8 @@ const Controls: React.FC<Props> = ({ dragColor, dragMass, play, addPlanet, clear
     const DEFAULT_DRAG_MASS = Math.round((dragMass / 10 ** 24) * 10 ** 10) / 10 ** 10;
 
     const [visible, setVisible] = useState(true);
+    const [dragVisible, setDragVisible] = useState(false);
+    const [formVisible, setFormVisible] = useState(false);
 
     const changeDragMass = (e: React.ChangeEvent<HTMLInputElement>) => {
         const value = Number(e.target.value);
@@ -67,7 +69,7 @@ const Controls: React.FC<Props> = ({ dragColor, dragMass, play, addPlanet, clear
     return (
         <div className='controls'>
             <button className='toggle-visible-button' onClick={() => setVisible(!visible)}>
-                <svg className={`arrow-icon ${!visible ? 'rotate-arrow' : null}`} width="104" height="92" viewBox="0 0 104 92" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <svg className={`arrow-icon ${visible ? 'rotate-arrow' : null}`} width="104" height="92" viewBox="0 0 104 92" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <rect x="95" width="10" height="100" rx="5" transform="rotate(30 95 0)" fill="white"/>
                     <rect y="5" width="10" height="100" rx="5" transform="rotate(-30 0 5)" fill="white"/>
                 </svg>
@@ -76,52 +78,55 @@ const Controls: React.FC<Props> = ({ dragColor, dragMass, play, addPlanet, clear
                 visible
                 ?
                 <section className='controls-section'>
-                    <section>
-                        <h1>Create Planet</h1>
-                        <form className='properties' onSubmit={(e) => createPlanet(e)}>
-                            <div className='inputs'>
-                                <input name='mass' placeholder='mass (10^24 kg)' type='number' required />
-                                <input name='radius' placeholder='radius' type='number' required />
-                                <input name='xPosition' placeholder='x position' type='number' required />
-                                <input name='yPosition' placeholder='y position' type='number' required />
-                                <input name='xVelocity' placeholder='x velocity' type='number' required />
-                                <input name='yVelocity' placeholder='y velocity' type='number' required />
-                                <input name='color' placeholder='color' type='text' required />
-                            </div>
-                            <button className='button create-planet-button' type='submit' >Create Planet</button>
-                        </form>
-                    </section>
-                    <section>
-                        <h1>Play Back</h1>
-                        <div className='play-back-buttons'>
-                            <button className='button' onClick={togglePlay}>{play ? 'Pause' : 'Play' }</button>
-                            <button className='button' onClick={clearPlanets}>Clear Planets</button>
-                        </div>
-                    </section>
-                    <section>
-                        <h1>Drag controls</h1>
-                        <section>
-                            <h2>Color</h2>
-                            <div className='color-buttons' onClick={e => changeDragColor(e)}>
-                                <button className={`red-button ${dragColor === 'red' ? 'selected-color' : null}`} name='red'></button>
-                                <button className={`green-button ${dragColor === 'green' ? 'selected-color' : null}`} name='green'></button>
-                                <button className={`blue-button ${dragColor === 'blue' ? 'selected-color' : null}`} name='blue'></button>
-                                <button className={`yellow-button ${dragColor === 'yellow' ? 'selected-color' : null}`} name='yellow'></button>
-                            </div>
+                    <div className='dropdowns'>
+                        <section className='drag-section'>
+                            <button className='drag-button' onClick={() => setDragVisible(!dragVisible)} >Drag Settings</button>
+                            {
+                                dragVisible
+                                ?
+                                    <div className='drag-settings'>
+                                        <section>
+                                            <h2>Color</h2>
+                                            <div className='color-buttons' onClick={e => changeDragColor(e)}>
+                                                <button className={`red-button ${dragColor === 'red' ? 'selected-color' : null}`} name='red'></button>
+                                                <button className={`green-button ${dragColor === 'green' ? 'selected-color' : null}`} name='green'></button>
+                                                <button className={`blue-button ${dragColor === 'blue' ? 'selected-color' : null}`} name='blue'></button>
+                                                <button className={`yellow-button ${dragColor === 'yellow' ? 'selected-color' : null}`} name='yellow'></button>
+                                            </div>
+                                        </section>
+                                        <section>
+                                            <h2>Mass (10^24 kg)</h2>
+                                            <input type='number' defaultValue={DEFAULT_DRAG_MASS} onChange={(e) => changeDragMass(e)} step='0.0000000001' />
+                                        </section>
+                                    </div>
+                                : null
+                            }
                         </section>
-                        <section>
-                            <h2>Mass (10^24 kg)</h2>
-                            <input type='number' defaultValue={DEFAULT_DRAG_MASS} onChange={(e) => changeDragMass(e)} step='0.0000000001' />
+                        <section className='form-section'>
+                            <button className='form-button' onClick={() => setFormVisible(!formVisible)} >Form</button>
+                            {
+                                formVisible
+                                ?
+                                    <form className='properties' onSubmit={(e) => createPlanet(e)}>
+                                        <input name='mass' placeholder='mass (10^24 kg)' type='number' required />
+                                        <input name='radius' placeholder='radius' type='number' required />
+                                        <input name='xPosition' placeholder='x position' type='number' required />
+                                        <input name='yPosition' placeholder='y position' type='number' required />
+                                        <input name='xVelocity' placeholder='x velocity' type='number' required />
+                                        <input name='yVelocity' placeholder='y velocity' type='number' required />
+                                        <input name='color' placeholder='color' type='text' required />
+                                        <button className='button create-planet-button' type='submit' >Create Planet</button>
+                                    </form>
+                                : null
+                            }
                         </section>
-                        <section>
-                            <h2>Planet</h2>
-                            <button onClick={() => setDragType('planet')}>Set To Planet</button>
-                        </section>
-                        <section>
-                            <h2>Sun</h2>
-                            <button onClick={() => setDragType('sun')}>Set To Sun</button>
-                        </section>
-                    </section>
+                    </div>
+                    <div className='buttons'>
+                        <button className='button' onClick={clearPlanets}>Clear</button>
+                        <button className='button' onClick={togglePlay}>{play ? 'Pause' : 'Play' }</button>
+                        <button onClick={() => setDragType('sun')}>Sun</button>
+                        <button onClick={() => setDragType('planet')}>Planet</button>
+                    </div>
                 </section>
                 : null
             }
